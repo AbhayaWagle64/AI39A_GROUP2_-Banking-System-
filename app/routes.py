3c80bb6cc236cae28224225e0baa22eab064dd5a
AbhayaWagle64 <quantumuniverse13@gmail.com>
2026-05-23 18:45:02 +0545

    Add profile management page and update routes

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def home():
     return render_template("index.html")
+
+@main.route("profile/management")
+def profile_manage():
+    return render_template("profile_management.html")
