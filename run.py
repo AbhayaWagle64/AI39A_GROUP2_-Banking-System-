74c3730629b3e9ca43627c3f0d169f18fabcf019
Sakina-17 <sakuuuumz@gmail.com>
2026-05-29 21:21:05 +0545

    Updated authentication frontend

--- run.py
+++ run.py
@@ -4,17 +4,26 @@
 
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder='app/templates',
+    static_folder='app/static'
+)
 
 @app.route('/login')
 def login():
-    return render_template('Login.html')
+    return render_template('login.html')
+
+
 @app.route('/')
 def register():
     return render_template('register.html')
+
+
 @app.route('/terms')
 def terms():
     return render_template('terms.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
