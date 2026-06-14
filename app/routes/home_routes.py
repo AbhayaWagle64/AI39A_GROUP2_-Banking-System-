65677dfa148ec242dc9abd0b20d87c75673da82d
Sakina-17 <sakuuuumz@gmail.com>
2026-06-14 22:28:20 +0545

    Remove files accidentally committed

--- app/routes/home_routes.py
+++
@@ -1,9 +0,0 @@
-# app/routes/home_routes.py
-# Root route for the web application.
-
-from flask import Blueprint
-from ..controllers.home_controller import show_home
-
-home_bp = Blueprint('home', __name__)
-
-home_bp.route('/', methods=['GET'])(show_home)
--- app/services/email_service.py
+++
@@ -1,7 +0,0 @@
-# app/services/email_service.py
-# Simple email service placeholder.
-
-
-def send_email(to_address, subject, body):
-    print(f'Sending email to {to_address} with subject {subject}')
-    return True
