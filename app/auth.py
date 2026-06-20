65468bd7a834c9562e734e301dda8917d085d075
Lakpa Sherpa <lakpa0101sherpa@gmail.com>
2026-06-20 07:44:58 +0545

    edit the auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, session, flash, request, jsonify
 from functools import wraps
 
-
+#  login
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -13,7 +13,7 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
-
+# admin required 
 def admin_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
